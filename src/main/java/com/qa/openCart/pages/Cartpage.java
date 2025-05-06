394568cf8ddde86a30e3c7ebcc46602dbed40f54
Jyothi <jo.kothuri@gmail.com>
2025-05-06 22:18:55 +0530

    Added as per ur review comments

--- src/main/java/com/qa/openCart/pages/Cartpage.java
+++ src/main/java/com/qa/openCart/pages/Cartpage.java
@@ -1,9 +1,12 @@
 package com.qa.openCart.pages;
 
+import org.openqa.selenium.By;
+
 public class Cartpage {
 int i=10;
+private By cart= By.id("cart");
 public void cart()
 {
-	System.out.println("Cart Method");
+	System.out.println("Cart Method" +cart);
 }
 }
